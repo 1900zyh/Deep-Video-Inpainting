62af5c64e404056a8bedb1ab3673c24f652fd286
1900zyh <zengyh7@mail2.sysu.edu.cn>
2019-10-26 22:17:11 +0800

    ftr: fixed random_shape as random_obj

--- utils/dataset.py
+++ utils/dataset.py
@@ -103,12 +103,11 @@ class dataset(data.Dataset):
       m = cv2.dilate(m, cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3)), iterations=4).astype(np.float32)
       return m
     elif self.mask_type == 'random_obj':
-      m_name = self.video_dict[video][i]
-      m = ZipReader.imread('../datazip/random_masks/{}/{}.zip'.format(self.data_name, video),\
-           '{}.png'.format(m_name.split('.jpg')[0])).resize((self.w, self.h))
+      m = ZipReader.imread('../datazip/random_masks/{}.zip'.format(self.data_name),\
+            '{}.png'.format(video)).resize((w, h))
       m = np.array(m)
       m = np.array(m>0).astype(np.uint8)
-      return m 
+      return m
     else:
       raise NotImplementedError(f"Mask type {self.mask_type} not exists")
 
